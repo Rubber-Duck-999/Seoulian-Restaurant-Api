16341ffb0e8ab8b55c53a78ddbc7d45fb1bf8828
Rubber-duck-99 <simon.crowther95@hotmail.co.uk>
2020-10-25 11:52:04 +0000

    Create initial lambda

---
+++ seoulian-restaurant-api/src/seoulian-restaurant-api/Function.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace seoulian_restaurant_api
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(NewRestaurant input, ILambdaContext context)
+        {
+            LambdaLogger.Log($"Calling function name: {context.FunctionName}\\n");
+            return $"Welcome: {input.name} {input.postcode}";
+        }
+    }
+}
---
+++ seoulian-restaurant-api/src/seoulian-restaurant-api/NewRestaurant.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace seoulian-restaurant-api
+{
+	public class NewRestaurant
+    {
+		public string name { get; set; }
+		public string postcode { get; set; }
+	}
+}
